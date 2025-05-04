d2eb7ed210df3d2c5aabe14164cf5624c17ebd2e
Abhinash Gupta <97685347+Abhinash01@users.noreply.github.com>
2025-05-04 17:52:06 +0000

    Scripting Done

---
+++ script.js
@@ -0,0 +1,8 @@
+const emojis = ["😀", "😎", "😴", "🤖", "🥳", "😡", "🧐", "😇", "💩", "👽", "🎃", "🐱‍👓"];
+const button = document.getElementById("emojiBtn");
+const display = document.getElementById("emojiDisplay");
+
+button.addEventListener("click", () => {
+  const index = Math.floor(Math.random() * emojis.length);
+  display.textContent = emojis[index];
+});
